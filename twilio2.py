8623432081bdc531bd1468a28efd8c4938a3442a
jgotting <johan@gotting.com>
2019-08-17 11:22:34 +0200

    second committ

--- twilio2.py
+++ twilio2.py
@@ -10,6 +10,6 @@ AUTH_TOKEN = "09f114f92bfa404cefc50d319a0d8d9a"
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 message = raw_input("Please enter your message: ")
 message = client.messages.create(to="+46761354501", from_="+46108844886", body=message)
-print message.sid
-print "Ditt meddelande har skickats på nätet!"
-print "Kolla din telefon är du snäll!"
+print(message.sid)
+print("Ditt meddelande har skickats på nätet!")
+print("Kolla din telefon är du snäll!")
